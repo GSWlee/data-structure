f3b58054636a9f59c0372224caf00615a612907c
GSWlee <995030368@qq.com>
2017-09-30 15:21:49 +0800

    基本排序

--- sorting.cpp
+++ sorting.cpp
@@ -12,7 +12,12 @@ void Swap(T&a, T&b)
 	b = temp;
 }
 template <class T>
-T* selectsort(T* a, int length)					//选择排序
+T findmin(T a,T b)
+{
+	return a<b?a:b;
+}
+template <class T>
+void selectsort(T* a, int length)					//选择排序
 {
 	for (int i = 0; i<length; i++)
 	{
@@ -24,20 +29,18 @@ T* selectsort(T* a, int length)					//选择排序
 		}
 		Swap(a[i], a[min]);
 	}
-	return a;
 }
 template <class T>
-T* insertsort(T* a, int length)					//插入排序
+void insertsort(T* a, int length)					//插入排序
 {
 	for (int i = 1; i<length; i++)
 	{
 		for (int j = i; j>0 && ifless(a[j], a[j - 1]); j--)
 			Swap(a[j], a[j - 1]);
 	}
-	return a;
 }
 template <class T>
-T* hillsort(T* a, int length)						//hill排序
+void hillsort(T* a, int length)						//hill排序
 {
 	int h = 1;
 	while (h<length / 3)
@@ -49,29 +52,55 @@ T* hillsort(T* a, int length)						//hill排序
 				Swap(a[j], a[j - h]);
 		h = h / 3;
 	}
-	return a;
 }
-template <class T>								   //归并排序，由于c++和java差异的问题造成的内存开销巨大问题正在解决
-void merge(T* a, int s, int t, int mid,int lo,int hi)
+template <class T>								   //归并排序
+void merge(T* a, int s, int t, int mid,int length)
 {
-	T* aue = new int[hi - lo+1];
+	T* aue = new int[length];
 	int i = s, j = mid + 1;
 	for (int k = s; k<=t; k++)
 		aue[k] = a[k];
 	for (int k = s; k<=t; k++)
 	{
-		if (i>mid)   					a[k] = aue[j++];
-		else if (j > t)   				a[k] = aue[i++];
+		if (i>mid)   					  a[k] = aue[j++];
+		else if (j > t)   				  a[k] = aue[i++];
 		else if (ifless(aue[i], aue[j]))  a[k] = aue[i++];
-		else							a[k] = aue[j++];
+		else							  a[k] = aue[j++];
+	}
+}
+template <class T>								 //自顶而下的归并
+void mergesort(T* a, int s, int t,int length)
+{
+	if ((t-s)<15)
+	{
+		selectsort(a,t-s+1);
+		return ;
+	}
+	int mid = (s + t) / 2;
+	mergesort(a, s, mid,length);
+	mergesort(a, mid + 1, t,length);
+	if(a[mid]<=a[mid+1])     return ;
+	merge(a, s, t, mid, lo,hi);
+}
+template <class T>								 //自底而上的归并
+void merge(T*a, T*aue, int s, int mid, int t)
+{
+	int i = s, j = mid + 1;
+	for (int k = s; k <= t; k++)
+		aue[k] = a[k];
+	for (int k = s; k <= t; k++)
+	{
+		if (i>mid)   					  a[k] = aue[j++];
+		else if (j > t)   				  a[k] = aue[i++];
+		else if (ifless(aue[i], aue[j]))  a[k] = aue[i++];
+		else							  a[k] = aue[j++];
 	}
 }
 template <class T>
-void mergesort(T* a, int s, int t,int lo,int hi)
+void mergesort(T*a, int length)
 {
-	if (t <= s)           return;
-	int mid = (s + t) / 2;
-	mergesort(a, s, mid,lo,hi);
-	mergesort(a, mid + 1, t,lo,hi);
-	merge(a, s, t, mid, lo,hi);
+	T* aue = new T[length];
+	for (int size = 1; size<length; size += size)
+		for (int lo = 0; lo<length - size; lo += size+size)
+			merge(a, aue, lo, lo + size - 1, findmin(lo + size + size - 1, length- 1));
 }
